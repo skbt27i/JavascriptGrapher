1bbc9c0aae7d33011d6e20c0871df49fc43c1c46
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-06-08 22:36:22 -0400

    Create form.js

---
+++ form.js
@@ -0,0 +1,34 @@
+
+//Sample code taken from https://reactjs.org/docs/forms.html
+// ** not mine in any way **
+
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Data point 1:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
