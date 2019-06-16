88bce3939f4beac2b7bfad7629902cbdd315f8b6
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-06-15 23:35:11 -0400

    Update form.js

--- form.js
+++ form.js
@@ -1,32 +1,32 @@
 
-//Sample code taken from https://reactjs.org/docs/forms.html
-// ** not mine in any way **
 
 
 class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-  }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
           Data point 1:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text"  />
         </label>
+      <label>
+          Data point 2:
+          <input type="text"  />
+        </label>
+      <label>
+          Data point 3:
+          <input type="text"  />
+        </label>
+      <label>
+          Data point 4:
+          <input type="text"  />
+        </label>
+      <label>
+          Data point 5:
+          <input type="text"  />
+        </label>
+ 
         <input type="submit" value="Submit" />
       </form>
     );
