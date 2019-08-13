dca74d27f9785d835c48121945dcf40e1db6b11f
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-08-12 21:58:59 -0400

    Update AppFile.js

--- AppFile.js
+++ AppFile.js
@@ -6,4 +6,15 @@ class App extends React.Component {
     	</div>
     );
   }	
+	
+   getData()
+	{
+		var data1=document.getElementById('data1').value;
+		var data2=document.getElementById('data2').value;
+		var data3=document.getElementById('data3').value;
+	        var data4=document.getElementById('data4').value;
+
+
+
+	}
 }
