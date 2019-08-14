d28f8b1fb1e9b9fb1b1dfc110c36c824d126a77f
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-08-14 18:42:22 -0400

    Update form.js

--- form.js
+++ form.js
@@ -9,11 +9,58 @@ class NameForm extends React.Component {
 		var data3=document.getElementById('data3').value;
 	  var data4=document.getElementById('data4').value;
 	}
+   
+   //from https://canvasjs.com/docs/charts/how-to/render-chart-by-accepting-datapoints-from-user-input/
+        window.onload = function() {
+            var dps = []; 
 
+            var chart = new CanvasJS.Chart("chartContainer", {
+                title: {
+                    text: "Linear Regression"
+                },
+                data: [{
+                    type: "line",
+                    dataPoints: dps1, dps2, dps3, dps4, dps5
+                }]
+            });
+
+            function addDataPointsAndRender() {
+                x1Value = Number(document.getElementById("xValue").value);
+                x2Value = Number(document.getElementById("yValue").value);
+		x3Value = Number(document.getElementById("xValue").value);
+                x4Value = Number(document.getElementById("yValue").value);
+		x5Value = Number(document.getElementById("yValue").value);
+
+                dps1.push({
+                    x1: xValue,
+                    y: yValue
+                });
+		    dps2.push({
+                    x2: xValue,
+                    y: yValue
+                });
+		    dps3.push({
+                    x3: xValue,
+                    y: yValue
+                });
+		    dps4.push({
+                    x4: xValue,
+                    y: yValue
+                });
+		    dps5.push({
+                    x5: xValue,
+                    y: yValue
+                });
+                chart.render();
+            }
+
+            var renderButton = document.getElementById("renderButton");
+            renderButton.addEventListener("click", addDataPointsAndRender);
+        }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} onclick={this.getData} />
+      <form onSubmit={this.handleSubmit} />
         <label>
           Data point 1:
           <input type="text"  />
@@ -36,7 +83,7 @@ class NameForm extends React.Component {
         </label>
  
       </form>
-	    <button onclick=onclick={this.getData}>Submit</button>
+	    <button onclick={this.getData} var="renderButton">Submit</button>
 
       
  
