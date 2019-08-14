2bd3c0b238d8c2b1a574067aadaab5902664ba83
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-08-13 20:05:40 -0400

    Update form.js

--- form.js
+++ form.js
@@ -2,10 +2,18 @@
 
 
 class NameForm extends React.Component {
+   getData()
+	{
+		var data1=document.getElementById('data1').value;
+		var data2=document.getElementById('data2').value;
+		var data3=document.getElementById('data3').value;
+	  var data4=document.getElementById('data4').value;
+	}
+
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} onclick="getData()" />
+      <form onSubmit={this.handleSubmit} onclick={this.getData} />
         <label>
           Data point 1:
           <input type="text"  />
