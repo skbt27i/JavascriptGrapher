7b884436a221e26861724e495b1d0f4fdccffc13
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-08-15 20:09:13 -0400

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,8 +1,33 @@
 
 
-import sys
+import sys, numpy as np
 from subprocess import Popen, PIPE
+
+from sklearn.linear_model import LinearRegression
+
 datas = [] 
 data = Popen(['node', 'AppData.js'], stdout=PIPE)
+
+#xDataPoint1 = 
+#xDataPoint2 = 
+#xDataPoint3 = 
+#xDataPoint4 = 
+#xDataPoint5 = 
+
+#yDataPoint1 = 
+#yDataPoint2 = 
+#yDataPoint3 = 
+#yDataPoint4 = 
+#yDataPoint5 = 
+
+x = np.array([xDataPoint1, xDataPoint2, xDataPoint3, xDataPoint4, xDataPoint5]).reshape((-1, 1))
+y = np.array([yDataPoint1, yDataPoint2, yDataPoint3, yDataPoint4, yDataPoint5)
+graph = LinearRegression()
+graph.fit(x, y)
+m = graph.coef_
+b = graph.intercept_
+
+rsquare = graph.score(x, y)
+
 while True:
     out = data.stdout.read(1)
