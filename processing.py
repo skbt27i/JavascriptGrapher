9e28aa50f71e5cf754a3ccec02c70c08eb0c91a0
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-08-19 18:28:57 -0400

    Update processing.py

--- processing.py
+++ processing.py
@@ -5,20 +5,29 @@ from subprocess import Popen, PIPE
 
 from sklearn.linear_model import LinearRegression
 
+# from https://dev.to/healeycodes/talking-to-python-from-javascript-and-back-again-31jf
 datas = [] 
 data = Popen(['node', 'AppData.js'], stdout=PIPE)
+buffer = b''
+dataPoint = data.stdout.read(1)
 
-#xDataPoint1 = 
-#xDataPoint2 = 
-#xDataPoint3 = 
-#xDataPoint4 = 
-#xDataPoint5 = 
+ if dataPoint == b'\n':
+        datas.append(float(buffer))
+        buffer = b''
+    else:
+        buffer += out 
 
-#yDataPoint1 = 
-#yDataPoint2 = 
-#yDataPoint3 = 
-#yDataPoint4 = 
-#yDataPoint5 = 
+xDataPoint1 = datas[0]
+xDataPoint2 = datas[1]
+xDataPoint3 = datas[2]
+xDataPoint4 = datas[3]
+xDataPoint5 = datas[4]
+
+yDataPoint1 = datas[5]
+yDataPoint2 = datas[6]
+yDataPoint3 = datas[7]
+yDataPoint4 = datas[8]
+yDataPoint5 = datas[9]
 
 x = np.array([xDataPoint1, xDataPoint2, xDataPoint3, xDataPoint4, xDataPoint5]).reshape((-1, 1))
 y = np.array([yDataPoint1, yDataPoint2, yDataPoint3, yDataPoint4, yDataPoint5)
