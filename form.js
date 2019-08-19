ca80a92a53adaa3c613a5ed2162ff0207a272307
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-08-19 18:33:06 -0400

    Update form.js

--- form.js
+++ form.js
@@ -4,10 +4,16 @@
 class NameForm extends React.Component {
    getData=()=>
 	{
-		var data1=document.getElementById('data1').value;
-		var data2=document.getElementById('data2').value;
-		var data3=document.getElementById('data3').value;
-	  var data4=document.getElementById('data4').value;
+		var xdata1=document.getElementById('data1').value;
+		var xdata2=document.getElementById('data2').value;
+		var xdata3=document.getElementById('data3').value;
+	        var xdata4=document.getElementById('data4').value;
+	   
+	   
+	        var ydata1=document.getElementById('data1').value;
+		var ydata2=document.getElementById('data2').value;
+		var ydata3=document.getElementById('data3').value;
+	        var ydata4=document.getElementById('data4').value;
 	}
    
    //from https://canvasjs.com/docs/charts/how-to/render-chart-by-accepting-datapoints-from-user-input/
@@ -30,26 +36,32 @@ class NameForm extends React.Component {
 		x3Value = Number(document.getElementById("xValue").value);
                 x4Value = Number(document.getElementById("yValue").value);
 		x5Value = Number(document.getElementById("yValue").value);
+		    
+		y1Value = Number(document.getElementById("xValue").value);
+                y2Value = Number(document.getElementById("yValue").value);
+		y3Value = Number(document.getElementById("xValue").value);
+                y4Value = Number(document.getElementById("yValue").value);
+		y5Value = Number(document.getElementById("yValue").value);
 
                 dps1.push({
                     x1: xValue,
-                    y: yValue
+                    y1: yValue
                 });
 		    dps2.push({
                     x2: xValue,
-                    y: yValue
+                    y2: yValue
                 });
 		    dps3.push({
                     x3: xValue,
-                    y: yValue
+                    y3: yValue
                 });
 		    dps4.push({
                     x4: xValue,
-                    y: yValue
+                    y4: yValue
                 });
 		    dps5.push({
                     x5: xValue,
-                    y: yValue
+                    y5: yValue
                 });
                 chart.render();
             }
@@ -62,23 +74,43 @@ class NameForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit} />
         <label>
-          Data point 1:
+          X Data point 1:
+          <input type="text"  />
+        </label>
+	  <label>
+          Y Data point 1:
           <input type="text"  />
         </label>
       <label>
-          Data point 2:
+          X Data point 2:
           <input type="text"  />
         </label>
       <label>
-          Data point 3:
+          Y Data point 2:
           <input type="text"  />
         </label>
       <label>
-          Data point 4:
+          X Data point 3:
+          <input type="text"  />
+        </label>
+     <label>
+          Y Data point 3:
           <input type="text"  />
         </label>
       <label>
-          Data point 5:
+          X Data point 4:
+          <input type="text"  />
+        </label>
+	<label>
+          Y Data point 4:
+          <input type="text"  />
+        </label>
+      <label>
+          X Data point 5:
+          <input type="text"  />
+        </label>
+	<label>
+          Y Data point 5:
           <input type="text"  />
         </label>
  
