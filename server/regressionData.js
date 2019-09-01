6057f3d5e8915e9550f0791a1f15251a959a9d7d
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2019-08-31 21:54:50 -0400

    Update regressionData.js

--- server/regressionData.js
+++ server/regressionData.js
@@ -1,1 +1,21 @@
+var spawn = require('child_process').spawn,
+    py = spawn('python', ['processing.py']),
+    inputString ='',
+    rSquareString='',
+    eqString ='',
+    resultString ='',
 
+py.stdout.on('data', function(inputString){
+  inputString += ' ',
+  resultString += inputString;
+});
+
+py.stdin.write(inputString);
+
+py.stdin.end();
+
+
+var lastStrings = resultString.split();
+
+rSquareString = lastStrings[0];
+eqString = lastStrings[1];
